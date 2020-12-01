eb2522ec7f3ac55ab7cc6658b5e396e6060dcbbe
kwachta <49271207+kwachta@users.noreply.github.com>
2020-12-01 20:34:44 +0100

    Add files via upload

---
+++ Cookie_stock_reader.js
@@ -0,0 +1,15 @@
+var stock=Game.Objects['Bank'].minigame;
+var n_of_stocks=0;
+for (var i in stock.goods)
+{
+  n_of_stocks+=1;
+}
+var str="";
+var prices = new Array(n_of_stocks);
+for (i=0;i<n_of_stocks;i++)
+{
+  prices[i]=stock.getGoodPrice(stock.goodsById[i]);
+  str=str+" "+prices[i];
+}
+str;
+
